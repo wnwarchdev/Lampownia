6433119e5c8ed74d0f560cd7dd6266e17d53fc49
Wojtek Nowak <billnewmann@gmail.com>
2020-09-25 16:45:07 +0100

    Add header style and navlinks

--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { NavLink } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
 
 // import { connect } from 'react-redux';
 // import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux.js';
@@ -8,8 +10,15 @@ import styles from './Header.module.scss';
 
 const Component = ({children}) => (
   <div className={styles.root}>
-    <h2>Header</h2>
-    {children}
+    <div className={styles.root}>
+      
+      <Button className={styles.logo} component={NavLink} exact to={`${process.env.PUBLIC_URL}/`} activeClassName='active'>LAMPOWNIA</Button> 
+      <div>
+        <Button className={styles.link} component={NavLink} to={`${process.env.PUBLIC_URL}/cart`} activeClassName='active'>CART</Button>
+        <Button className={styles.link} component={NavLink} to={`${process.env.PUBLIC_URL}/product`} activeClassName='active' >PRODUCT</Button>
+        <Button className={styles.link} component={NavLink} to={`${process.env.PUBLIC_URL}/sth`} activeClassName='active' >NFOUND</Button>
+      </div>
+    </div>
   </div>
 );
 
