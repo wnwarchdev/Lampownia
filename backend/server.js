3b0edef3209f292060c522f6530aaa3bc65f7fc8
Wojtek Nowak <billnewmann@gmail.com>
2020-10-07 22:37:20 +0100

    Install and add helmet to server

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
+const helmet = require('helmet');
 
 
 const router = require('./routes/products.routes');
@@ -16,6 +17,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(helmet());
 
 
 /* API ENDPOINTS */
